f4dcd93d0c588683260bdfae2c3e5261bfc55dfa
chenshenhai <cshenhai@gmail.com>
2019-06-26 23:29:55 +0800

    feat: update filter person

--- src/service/filter/person.ts
+++ src/service/filter/person.ts
@@ -1,11 +1,11 @@
 import { FilterOpts } from './../../core/filter/filter';
-import { filterPersonImageData } from '../../core/filter/person';
+import { filterPersonImageData, filterPersonWhiteSkinImageData } from '../../core/filter/person';
 import { asyncWorker } from '../../util/worker';
 import schemaParser from '../schema-parser';
 
 export const filterPerson = function(opts: FilterOpts): Promise<any> {
   return new Promise(function(resolve, reject) {
-    asyncWorker(filterPersonImageData, opts).then(function(result: ImageData) {
+    asyncWorker(filterPersonWhiteSkinImageData, opts).then(function(result: ImageData) {
       const newSchema = schemaParser.parseImageDataToSchema(result);
       resolve(newSchema)
     }).catch(function(err){

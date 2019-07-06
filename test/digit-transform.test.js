bdf8880360c7599a6763bf6596e6b9fc358ed1b2
chenshenhai <cshenhai@gmail.com>
2019-07-06 20:33:00 +0800

    test: update testing of Pictool.digit.transform

--- test/digit-transform.test.js
+++ test/digit-transform.test.js
@@ -4,12 +4,20 @@ const expect = chai.expect
 
 const transform = digit.transform;
 
-describe( 'test: core-digit-transform', ( ) => {
+describe( 'test: Pictool.digit.transform', ( ) => {
   it('transform.RGB2HSL', ( done ) => {
     const rgb = {r: 12, g: 15, b: 10};
     const hsl = transform.RGB2HSL(rgb);
     const expectResult = {h:96, s:20, l:5};
     expect(hsl).to.deep.equal(expectResult)
+    done()
+  });
+
+  it('transform.HSL2RGB', ( done ) => {
+    const hsl = {h:96, s:20, l:5} ;
+    const rgb = transform.HSL2RGB(hsl);
+    const expectResult = {r: 12, g: 15, b: 10};
+    expect(rgb).to.deep.equal(expectResult)
     done()
   })
 })

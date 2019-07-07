88373b72ceecdfba4cc52c63150fdbb0d67c9985
chenshenhai <cshenhai@gmail.com>
2019-07-07 22:15:17 +0800

    test: add testing of digit.algorithm.grayscale

--- test/digit-algorithm.test.js
+++ test/digit-algorithm.test.js
@@ -1,19 +1,27 @@
 const chai = require('chai');
 const digit = require('../dist/digit');
 const expect = chai.expect
-
 const algorithm = digit.algorithm;
-const imgData = {
-  data: []
-}
+const DigitImageData = digit.DigitImageData;
+
+const img = require('./data/image-data-origin.json');
+const imgGrayscale = require('./data/image-data-grayscrale.json');
 
 describe( 'test: Pictool.digit.algorithm', ( ) => {
 
   it('algorithm.grayscale', ( done ) => {
-    const rgb = {r: 12, g: 15, b: 10};
-    const hsl = algorithm.RGB2HSL(rgb);
-    const expectResult = {h:96, s:20, l:5};
-    expect(hsl).to.deep.equal(expectResult)
+
+    const digitImg = new DigitImageData({width: img.width, height: img.height});
+    digitImg.setData(img.data);
+    const digitImgGrayscale = algorithm.grayscale(digitImg);
+    
+    expect(digitImgGrayscale.width).to.deep.equal(imgGrayscale.width);
+    expect(digitImgGrayscale.height).to.deep.equal(imgGrayscale.height);
+    imgGrayscale.data.forEach(function(num, i) {
+      // console.log(`expect index is: ${i}`)
+      expect(digitImgGrayscale.data[i]).to.deep.equal(num);
+    });
+
     done()
   });
 

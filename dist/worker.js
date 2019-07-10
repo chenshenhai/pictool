9fdead34e7f966adc2cb5dfdfd4e29396f15d641
chenshenhai <cshenhai@gmail.com>
2019-07-10 23:38:27 +0800

    test: add testing of process.hue

--- dist/worker.js
+++ dist/worker.js
@@ -32,6 +32,7 @@
       };
       return DigitImageData;
   }());
+  //# sourceMappingURL=digit-image-data.js.map
 
   var grayscale = function (imgData) {
       var width = imgData.width, height = imgData.height, data = imgData.data;
@@ -50,6 +51,7 @@
       }
       return digitImg;
   };
+  //# sourceMappingURL=grayscale.js.map
 
   // Thanks to https://github.com/miguelmota/sobel/
   function imgDataAt(digitData, x, y) {
@@ -107,6 +109,7 @@
       grayImg = null;
       return digitImg;
   };
+  //# sourceMappingURL=sobel.js.map
 
   var RGBA_MID = 255 / 2;
   var RGBA_MAX = 255;
@@ -114,6 +117,7 @@
   var H_MAX = 360;
   var S_MAX = 100;
   var L_MAX = 100;
+  //# sourceMappingURL=static.js.map
 
   var invert = function (imgData) {
       var width = imgData.width, height = imgData.height, data = imgData.data;
@@ -131,6 +135,7 @@
       }
       return digitImg;
   };
+  //# sourceMappingURL=invert.js.map
 
   /*! *****************************************************************************
   Copyright (c) Microsoft Corporation. All rights reserved.
@@ -232,6 +237,7 @@
       }
       return { r: r, g: g, b: b };
   };
+  //# sourceMappingURL=hsl2rgb.js.map
 
   var parseRGBNum = function (origin) {
       return origin * 100 / RGBA_MAX; // [1, 100]
@@ -351,6 +357,7 @@
       }
       return { h: h, s: s, l: l };
   };
+  //# sourceMappingURL=rgb2hsl.js.map
 
   var transformDigitImageData = function (digitImageData, opts) {
       var data = digitImageData.data, width = digitImageData.width, height = digitImageData.height;
@@ -373,6 +380,7 @@
       digitImageData = null;
       return rsImageData;
   };
+  //# sourceMappingURL=index.js.map
 
   var hue = function (imgData, opts) {
       var width = imgData.width, height = imgData.height, data = imgData.data;
@@ -389,6 +397,7 @@
       digitImg = transformDigitImageData(digitImg, { percent: percent, value: value });
       return digitImg;
   };
+  //# sourceMappingURL=hue.js.map
 
   var lightness = function (imgData, opts) {
       var width = imgData.width, height = imgData.height, data = imgData.data;
@@ -405,6 +414,7 @@
       digitImg = transformDigitImageData(digitImg, { percent: percent, value: value });
       return digitImg;
   };
+  //# sourceMappingURL=lightness.js.map
 
   var saturation = function (imgData, opts) {
       var width = imgData.width, height = imgData.height, data = imgData.data;
@@ -421,15 +431,17 @@
       digitImg = transformDigitImageData(digitImg, { percent: percent, value: value });
       return digitImg;
   };
+  //# sourceMappingURL=saturation.js.map
 
   var process = {
       grayscale: grayscale,
       sobel: sobel,
       invert: invert,
       hue: hue,
-      lightness: lightness,
       saturation: saturation,
+      lightness: lightness,
   };
+  //# sourceMappingURL=index.js.map
 
   var digitImageData2ImageData = function (digitImgData) {
       var data = digitImgData.data, width = digitImgData.width, height = digitImgData.height;
@@ -439,6 +451,7 @@
       });
       return imgData;
   };
+  //# sourceMappingURL=image-data.js.map
 
   var Effect = /** @class */ (function () {
       function Effect(imageData) {
@@ -467,6 +480,7 @@
       };
       return Effect;
   }());
+  //# sourceMappingURL=index.js.map
 
   var origin = function (opts) {
       var imageData = opts.imageData;
@@ -522,6 +536,7 @@
       var rsImageData = effect.process('saturation', { percent: 76 }).getImageData();
       return rsImageData;
   };
+  //# sourceMappingURL=index.js.map
 
   var filterMap = /*#__PURE__*/Object.freeze({
     origin: origin,
@@ -543,6 +558,7 @@
           'result': result
       }, undefined);
   };
+  //# sourceMappingURL=worker.js.map
 
 }());
 //# sourceMappingURL=worker.js.map
--- test/digit-process.test.js
+++ test/digit-process.test.js
@@ -8,6 +8,8 @@ const img = require('./data/image-data-origin.json');
 const imgGrayscale = require('./data/image-data-grayscrale.json');
 const imgInvert = require('./data/image-data-invert.json');
 const imgSobel = require('./data/image-data-sobel.json');
+const imgHueVal180 =  require('./data/image-data-hue.val.180.json');
+const imgHuePer75 =  require('./data/image-data-hue.per.75.json');
 
 describe( 'test: Pictool.digit.process', ( ) => {
 
@@ -52,6 +54,40 @@ describe( 'test: Pictool.digit.process', ( ) => {
     digitImg.setData(img.data);
     const digitImgRs = process.sobel(digitImg);
     const expectImg = imgSobel;
+    
+    expect(digitImgRs.width).to.deep.equal(expectImg.width);
+    expect(digitImgRs.height).to.deep.equal(expectImg.height);
+    expectImg.data.forEach(function(num, i) {
+      // console.log(`expect index is: ${i}`)
+      expect(digitImgRs.data[i]).to.deep.equal(num);
+    });
+
+    done()
+  });
+
+  it('process.hue({value: 180})', ( done ) => {
+
+    const digitImg = new DigitImageData({width: img.width, height: img.height});
+    digitImg.setData(img.data);
+    const digitImgRs = process.hue(digitImg, {value: 180});
+    const expectImg = imgHueVal180;
+    
+    expect(digitImgRs.width).to.deep.equal(expectImg.width);
+    expect(digitImgRs.height).to.deep.equal(expectImg.height);
+    expectImg.data.forEach(function(num, i) {
+      // console.log(`expect index is: ${i}`)
+      expect(digitImgRs.data[i]).to.deep.equal(num);
+    });
+
+    done()
+  });
+
+  it('process.hue({percent: 75})', ( done ) => {
+
+    const digitImg = new DigitImageData({width: img.width, height: img.height});
+    digitImg.setData(img.data);
+    const digitImgRs = process.hue(digitImg, {percent: 75});
+    const expectImg = imgHuePer75;
     
     expect(digitImgRs.width).to.deep.equal(expectImg.width);
     expect(digitImgRs.height).to.deep.equal(expectImg.height);

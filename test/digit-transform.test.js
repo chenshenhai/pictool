ebb286a123592dd32df6c4ef0a73a11e57f137c8
chenshenhai <cshenhai@gmail.com>
2019-07-21 01:28:46 +0800

    test: update testing of transform

--- test/digit-transform.test.js
+++ test/digit-transform.test.js
@@ -1,23 +1,23 @@
-// const chai = require('chai');
-// const digit = require('../dist/digit');
-// const expect = chai.expect
+const chai = require('chai');
+const digit = require('../dist/digit');
+const expect = chai.expect
 
-// const transform = digit.transform;
+const transform = digit.transform;
 
-// describe( 'test: Pictool.digit.transform', ( ) => {
-//   it('transform.RGB2HSL', ( done ) => {
-//     const rgb = {r: 12, g: 15, b: 10};
-//     const hsl = transform.RGB2HSL(rgb);
-//     const expectResult = {h:96, s:20, l:5};
-//     expect(hsl).to.deep.equal(expectResult)
-//     done()
-//   });
+describe( 'test: Pictool.digit.transform', ( ) => {
+  it('transform.RGB2HSL', ( done ) => {
+    const rgb = {r: 12, g: 15, b: 10};
+    const hsl = transform.RGB2HSL(rgb);
+    const expectResult = {h:96, s:20, l:5};
+    expect(hsl).to.deep.equal(expectResult)
+    done()
+  });
 
-//   it('transform.HSL2RGB', ( done ) => {
-//     const hsl = {h:96, s:20, l:5} ;
-//     const rgb = transform.HSL2RGB(hsl);
-//     const expectResult = {r: 12, g: 15, b: 10};
-//     expect(rgb).to.deep.equal(expectResult)
-//     done()
-//   })
-// })
+  it('transform.HSL2RGB', ( done ) => {
+    const hsl = {h:96, s:20, l:5} ;
+    const rgb = transform.HSL2RGB(hsl);
+    const expectResult = {r: 12, g: 15, b: 10};
+    expect(rgb).to.deep.equal(expectResult)
+    done()
+  })
+})

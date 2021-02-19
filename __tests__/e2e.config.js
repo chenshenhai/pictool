002133d6cee1db30922074cdb434a6dc8e531add
chenshenhai <cshenhai@gmail.com>
2021-02-19 22:52:20 +0800

    chore: init e2e testing

---
+++ __tests__/e2e.config.js
@@ -0,0 +1,16 @@
+const exampleModuleList = [
+  'browser-sandbox-alpha.html',
+  'browser-sandbox-saturation.html',
+  'digit-process-gamma.html',
+];
+
+const port = 3001;
+const width = 400;
+const height = 600;
+
+module.exports = {
+  exampleModuleList,
+  port,
+  width,
+  height,
+}
---
+++ __tests__/screenshot.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const serveHandler = require('serve-handler');
+const jimp = require('jimp');
+const compose = require('koa-compose');
+const { exampleModuleList, port, width, height, } = require('./e2e.config');
+
+
+const screenDir = path.join(__dirname, 'screenshot');
+
+
+async function main() {
+
+  const server = http.createServer((req, res) => serveHandler(req, res, {
+    public: path.join(__dirname, '..'),
+  }));
+  server.listen(port, async () => {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setViewport( { width: width, height: height } );
+
+      const tasks = [];
+      exampleModuleList.forEach((mod) => {
+        const name = mod.replace(/.html$/, '');
+        const pagePath = `/example/module/${mod}`;
+        tasks.push(async (ctx, next) => {
+          await page.goto(`http://127.0.0.1:${port}${pagePath}`);
+          const buf = await page.screenshot();
+          const screenPicPath = path.join(screenDir, `${name}.jpg`);
+          (await jimp.read(buf)).scale(1).quality(100).write(screenPicPath);
+          console.log(`create screenshot [${name}] scuccess.`) ;
+          await next();
+        })
+      });
+      await compose(tasks)();
+      
+      await browser.close();
+      server.close();
+    } catch (err) {
+      server.close();
+      console.error(err);
+      process.exit(-1);
+    }
+  });
+  server.on('SIGINT', () => process.exit(1) );
+}
+
+main();
